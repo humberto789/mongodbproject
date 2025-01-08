7af6cf5f25ade0190d3e55c8cf5d9d89b1261728
MICHELLE MARTINS <michelle.teixeira.124@ufrn.edu.br>
2025-01-08 15:09:08 -0300

    add delete request

---
+++ src/main/java/com/imd/mongodbproject/runner/DataLoader.java
@@ -0,0 +1,18 @@
+package com.example.mongodbproject;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Bean
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
